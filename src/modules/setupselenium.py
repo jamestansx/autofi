a1a88e6f51fa8fa915f583a0bab9296ce929957b
James Tan <jamestansx@gmail.com>
2021-03-21 01:15:56 +0800

    Add setting up selenium driver

---
+++ src/modules/setupselenium.py
@@ -0,0 +1,25 @@
+import sys
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+import wifiauth
+
+
+def setupSelenium(driverPath):
+    if wifiauth.is_Connected():
+        chrome_option = Options()
+        chrome_option.add_experimental_option("excludeSwitches", ["enable-logging"])
+        # chrome_option.add_argument("--headless")
+        chrome_option.add_argument("--ignore-certificate-errors")
+        chrome_option.add_argument("--ignore-ssl-errors")
+        chrome_option.add_argument("--no-sandbox")
+        driver = webdriver.Chrome(executable_path=driverPath, options=chrome_option)
+        driver.implicitly_wait(3)
+        return driver
+
+
+def open_webdriver(driver, authUrl):
+    try:
+        driver.get(authUrl)
+    except Exception:
+        sys.exit(1)
