ebe00d070b876aa4957fa39d8f5f84847e3a5fde
James Tan <jamestansx@gmail.com>
2021-03-22 02:56:08 +0800

    Add matching WiFi name

--- src/main.py
+++ src/main.py
@@ -1,9 +1,21 @@
-import os
-import sys
+import os, sys, subprocess
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from src.modules import config, setupselenium, wifiauth, wifibot, argcli
+from src.modules.settings import log
 
+def isUtemWifi(wifiname="Kediaman_Pelajar"):
+    if wifiname in check_wifiName():
+        return True
+    else:
+        return False
+
+def check_wifiName(status:bool=True):
+    if status:
+        return str(subprocess.check_output("netsh wlan show interfaces"))
+    else:
+        connected_ssid = str(subprocess.check_output("powershell.exe (get-netconnectionProfile).Name", shell=True).strip())
+        return connected_ssid.strip("b'")
 
 def setup(args):
     webdriverPath, username, password, url, isFirstRun = config.getSettings()
@@ -23,5 +35,10 @@ def main(driver, username, password, url, args):
 
 if __name__ == "__main__":
     args = argcli.arg_cli()
-    driver, username, password, url = setup(args)
-    main(driver, username, password, url, args)
+    if isUtemWifi():
+        driver, username, password, url = setup(args)
+        main(driver, username, password, url, args)
+    else:
+        logger = log.newLogging("log.log", isDebug = args.isDebug)
+        connected_ssid = check_wifiName(isUtemWifi())
+        logger.warning(f"Connected WiFi is {connected_ssid}...")
