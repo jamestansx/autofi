a300cc1eef1262c932e666f28b691bf014f6c5e5
James Tan <jamestansx@gmail.com>
2021-03-21 17:11:42 +0800

    Add newline for each log

--- src/modules/wifiauth.py
+++ src/modules/wifiauth.py
@@ -3,12 +3,21 @@ import urllib.request as request
 from src.modules.settings import log
 
 
-def is_Connected(url="https://stackoverflow.com/", timeout=3, isDebug = False):
-    logger = log.newLogging("log.log", isDebug = isDebug)
-    try:
-        request.urlopen(url, timeout=timeout)
-        logger.info("WiFi is connected and accessible.")
-        return True
-    except Exception as e:
-        logger.info(f"Failed to connect - attempting to login - \n{e}")
-        return False
+def is_Connected(url="https://stackoverflow.com/", timeout=5, isDebug=False):
+    logger = log.newLogging("log.log", isDebug=isDebug)
+    attempts = 0
+    while True & attempts < 3:
+        try:
+            attempts += 1
+            request.urlopen(url, timeout=timeout)
+            logger.info("WiFi is connected and accessible.")
+            return True
+        except Exception as e:
+            if attempts == 2:
+                logger.error("Last trial==>Too many trial!!")
+            if str(e) in "urlopen error [Errno 11001] getaddrinfo failed":
+                logger.warn(f"Failed to open url - attempting to reopen - \n{e}\n")
+                continue
+            else:
+                logger.info(f"WiFi is not authenticated -- attempting to authenticate -- \n{e}\n")
+                return False
--- src/modules/wifibot.py
+++ src/modules/wifibot.py
@@ -13,10 +13,10 @@ def bot(driver, username, password, isDebug =False):
         if isSuccess:
             driver.quit()
         else:
-            logger.critical(f"Failed to authenticate--\n{isSuccess}")
+            logger.critical(f"Failed to authenticate--\n{isSuccess}\n")
             driver.quit()
     except Exception as e:
-        logger.error(f"{e}")
+        logger.error(f"{e}\n")
         driver.quit()
 
 
@@ -24,22 +24,22 @@ def bot(driver, username, password, isDebug =False):
 def input_username(driver, username, logger):
     user_elem = driver.find_element_by_css_selector("#ID_formc5279f78_weblogin_user")
     user_elem.send_keys(username)
-    logger.info("username is entered")
+    logger.info("username is entered\n")
 
 def input_password(driver, password, logger):
     pass_elem = driver.find_element_by_css_selector("#ID_formc5279f78_weblogin_password")
     pass_elem.send_keys(password)
-    logger.info("password is entered")
+    logger.info("password is entered\n")
 
 def click_button(driver, logger):
     button = driver.find_element_by_css_selector("#ID_formc5279f78_weblogin_submit")
     button.click()
-    logger.info("submit button is clicked")
+    logger.info("submit button is clicked\n")
 
 def is_success(driver, logger):
     try:
         driver.find_element_by_css_selector("body > font > b")
-        logger.info("WiFi is successfully authenticated")
+        logger.info("WiFi is successfully authenticated\n")
         return True
     except Exception as e:
         return e
