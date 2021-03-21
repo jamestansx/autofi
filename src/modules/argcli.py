1d58587bc65cb7381661ccc2f80cd7612f16eea0
James Tan <jamestansx@gmail.com>
2021-03-21 15:17:21 +0800

    Add argparse

---
+++ src/modules/argcli.py
@@ -0,0 +1,13 @@
+import argparse
+
+
+def arg_cli():
+    parser = argparse.ArgumentParser(
+        prog="AutoFi-UTeM", description="A bot to automatically authenticate UTeM WiFi"
+    )
+    parser.add_argument(
+        "--debug", "-d", dest="debug", action="store_true", default=False, help="Enable debug mode"
+    )
+    args = parser.parse_args()
+
+    return args
