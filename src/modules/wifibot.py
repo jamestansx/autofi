4ec081423c48f18996cdb3678ce81de268483f36
James Tan <jamestansx@gmail.com>
2021-04-04 22:26:53 +0800

    Add test to confirm connection success

--- src/modules/wifibot.py
+++ src/modules/wifibot.py
@@ -1,15 +1,17 @@
 from selenium.common.exceptions import StaleElementReferenceException, NoSuchElementException
 from src.modules.settings import log
+from src.modules import wifiauth
 
 ignore_exceptions = (StaleElementReferenceException, NoSuchElementException)
 
-def bot(driver, username, password, isDebug =False):
+
+def bot(driver, username, password, isDebug=False):
     try:
-        logger = log.newLogging("log.log", isDebug = isDebug)
+        logger = log.newLogging("log.log", isDebug=isDebug)
         input_username(driver, username, logger)
         input_password(driver, password, logger)
         click_button(driver, logger)
-        isSuccess = is_success(driver, logger)
+        isSuccess = wifiauth.is_Connected()
         if isSuccess:
             if isDebug:
                 input("Press enter to continue")
@@ -19,10 +21,9 @@ def bot(driver, username, password, isDebug =False):
             if isDebug:
                 input("Press enter to continue")
             driver.quit()
-    except Exception as e:
-        logger.error(f"{e}\n")
+    except:
+        logger.exception(f"Exception: ")
         driver.quit()
-
 
 
 def input_username(driver, username, logger):
@@ -30,20 +31,14 @@ def input_username(driver, username, logger):
     user_elem.send_keys(username)
     logger.info("username is entered\n")
 
+
 def input_password(driver, password, logger):
     pass_elem = driver.find_element_by_css_selector("#ID_formc5279f78_weblogin_password")
     pass_elem.send_keys(password)
     logger.info("password is entered\n")
 
+
 def click_button(driver, logger):
     button = driver.find_element_by_css_selector("#ID_formc5279f78_weblogin_submit")
     button.click()
     logger.info("submit button is clicked\n")
-
-def is_success(driver, logger):
-    try:
-        driver.find_element_by_css_selector("body > font > b")
-        logger.info("WiFi is successfully authenticated\n")
-        return True
-    except Exception as e:
-        return e
