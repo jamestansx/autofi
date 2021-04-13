f05e1679646ae16326bfadbd9c9c27cb35518434
James Tan <jamestansx@gmail.com>
2021-04-14 03:21:30 +0800

    Fix minor bug

--- src/modules/config.py
+++ src/modules/config.py
@@ -55,7 +55,7 @@ def editSetting(userData_json):
     if choice in {"4", "all"}:
         data["url"] = Prompt.ask("[italic green]New url")
     if choice in {"5", "all"}:
-        mainPath, password = getSettings()
+        mainPath, password = getTaskInfo()
         create_task(mainPath, password)
     if choice in {"6", "all"}:
         console.print(
