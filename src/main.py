a5948e5234392b2ac338275f65ca7a5f686caa58
James Tan <jamestansx@gmail.com>
2021-07-20 23:49:28 +0800

    Remove redundant comments

--- src/main.py
+++ src/main.py
@@ -3,9 +3,6 @@ from subprocess import Popen, check_output
 from modules.argcli import arg_cli
 from modules.settings.log import newLogging
 
-# from modules.config import getSettings
-# setupselenium, wifiauth
-# import sys
 
 
 def isUtemWifi(wifiname="Kediaman_Pelajar"):
@@ -17,32 +14,15 @@ def check_wifiName(status: bool = True):
     if status:
         return str(check_output("netsh wlan show interfaces"))
     connected_ssid = str(
-        check_output("powershell.exe (get-netconnectionProfile).Name", shell=True).strip()
+        check_output(
+            "powershell.exe (get-netconnectionProfile).Name", shell=True
+        ).strip()
     )
     return connected_ssid.strip("b'")
 
 
-"""
-def setup(args):
-    return getSettings()
-    # ! Do not activate chrome driver as it will clog up the memory
-    # ! as it doesn't close properly
-    # webdriverPath, username, password, url, isFirstRun = config.getSettings()
-    # if isFirstRun:
-        # sys.exit(1)
-    # try:
-        # driver = setupselenium.setupSelenium(
-        # webdriverPath, wifiauth.is_Connected(isDebug=args.isDebug), args.isDebug
-        # )
-        # return driver, username, password, url
-    # except Exception:
-        # return None, None, None, None
-"""
 
-
-def main():  # driver, username, password, url, args):
-    # setupselenium.open_webdriver(driver, url)
-    # wifibot.bot(driver, username, password, args.isDebug)
+def main():
     # FUCK I don't know about cURL and spending my time to do this bot
     # when in fact it can be done with this command
     return Popen(
@@ -53,7 +33,6 @@ def main():  # driver, username, password, url, args):
 if __name__ == "__main__":
     args = arg_cli()
     if isUtemWifi():
-        # driver, username, password, url, isFirstRun = setup(args)
         main()
     else:
         logger = newLogging("log.log", isDebug=args.isDebug)
